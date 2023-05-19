ae0e01e8293a9586c8ac28beab84423d7e4bd8cf
Pritish Karmakar <pritishkarmakar17@gmail.com>
2023-05-19 12:52:22 +0530

    modified May 19

--- fft_gaussian.py
+++ fft_gaussian.py
@@ -10,16 +10,16 @@ def ft(y):
     int_im = lambda t: f(t)*np.cos(y*t)
     g_re = quad(int_re,-np.inf,np.inf)[0]/(2*np.pi)
     g_im = quad(int_im,-np.inf,np.inf)[0]/(2*np.pi)
-    return g_re + 1j*g_im
+    return g_re - 1j*g_im
 g = np.frompyfunc(ft, 1, 1)
 
 xv = np.linspace(-7,7,1000)
 yv = np.abs(g(xv))
 plt.plot(xv, yv, lw=1)
 plt.xlabel("$\omega$")
-plt.ylabel("$g(\omega)$")
+plt.ylabel("$abs(g(\omega))$")
 plt.grid(True)
-
+ 
 import tikzplotlib
 tikzplotlib.clean_figure()
 tikzplotlib.save("fft_gaussian.tex")
