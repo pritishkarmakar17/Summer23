5aef6630ce1ee38bb5520e9476df47ca390c0d5a
Pritish Karmakar <pritishkarmakar17@gmail.com>
2023-05-21 14:12:56 +0530

    modified May 21

--- intensity_gaussian.py
+++ intensity_gaussian.py
@@ -4,8 +4,8 @@ mpl.rcParams['figure.dpi'] = 150
 import numpy as np
 import matplotlib.pyplot as plt
 
-R=lambda r: np.exp(-r**2/2)
-a1=np.linspace(-3.5,3.5,200)
+R=lambda r: np.exp(-2*r**2)
+a1=np.linspace(-1.7,1.7,200)
 xv,yv=np.meshgrid(a1,a1)
 zv=R(np.sqrt(xv**2+yv**2))
 plt.contourf(xv,yv,zv,levels=100,cmap='viridis')
@@ -13,5 +13,10 @@ plt.xlabel("x")
 plt.ylabel("y")
 plt.colorbar()
 
+theta=np.linspace(0,2*np.pi,500)
+x=np.cos(theta)
+y=np.sin(theta)
+plt.plot( x, y, "--", lw=0.8)
 
-plt.show()
+#plt.show()
+plt.savefig("intensity.png")
