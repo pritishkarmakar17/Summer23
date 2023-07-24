fdeebaa7cc441d382b909ad3a616880d892eaded
Pritish Karmakar <pritishkarmakar17@gmail.com>
2023-07-25 02:43:09 +0530

    Modified Jul 25

--- R_vs_z.py
+++ R_vs_z.py
@@ -5,16 +5,18 @@ plt.style.use("classic")
 def f(t):
     return t + 1/t
 
-xv = np.linspace(0,6,1000)
+xv = np.linspace(0,50,1000)
 yv = f(xv)
-plt.plot(xv, yv, lw=3)
+plt.plot(xv, yv, lw=1)
 plt.xlabel("$z$")
 plt.ylabel("$R(z)$")
 #plt.grid(True)
-plt.ylim(-0.1,7)
-tics=[0,1,2,3,4,5,6]
-plt.xticks(tics, ["$0$","$z_0$","$2z_0$","$3z_0$","$4z_0$","$5z_0$","$6z_0$"] )
-plt.savefig("R_vs_z beamer.png")
+#plt.ylim(-0.1,7)
+#tics=[0,1,2,3,4,5,6]
+#plt.xticks(tics, ["$0$","$z_0$","$2z_0$","$3z_0$","$4z_0$","$5z_0$","$6z_0$"] )
+#plt.savefig("R_vs_z beamer.png")
+
+plt.show()
 
 #import tikzplotlib
 #tikzplotlib.clean_figure()
--- rad of curve.py
+++ rad of curve.py
@@ -8,9 +8,11 @@ w_0=0.2
 l=4*10**(-3)
 z_0=np.pi*w_0**2/l
 k=2*np.pi/l
+xl=100
+
 # define intensity 
-R=lambda x,z: np.exp(-x**2/(w_0**2*(1+z**2/z_0**2)))/np.sqrt(1+z**2/z_0**2)*(np.sin(np.arctan(z/z_0)-0.5*x**2* k/(z+z_0/z)-k*z))
-a1=np.linspace(-50,50,1000)
+R=lambda x,z: np.exp(-x**2/(w_0**2*(1+z**2/z_0**2)))/np.sqrt(1+z**2/z_0**2)*(np.sin(np.arctan(z/z_0)-0.5*x**2* k/(z+z_0**2/z)-k*z))
+a1=np.linspace(-xl,xl,1000)
 a2=np.linspace(-1,1,500)
 xv,zv=np.meshgrid(a2,a1)
 I=R(xv,zv)
@@ -20,5 +22,5 @@ plt.xlabel("z")
 plt.ylabel("x-y plane")
 
 
-
-plt.savefig("Rad of cur beamer.png")
+plt.show()
+#plt.savefig("Rad of cur beamer.png")
