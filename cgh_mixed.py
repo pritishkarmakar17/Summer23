954df0186df5abcbdfad8960c55fc57ed4a0b0a4
Pritish Karmakar <pritishkarmakar17@gmail.com>
2023-07-28 14:09:49 +0530

    Modfied Jul 28

---
+++ cgh_mixed.py
@@ -0,0 +1,36 @@
+import matplotlib as mpl
+mpl.rcParams['figure.dpi'] = 150
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+l=2
+
+# define phi of range 0 to 2*pi
+def taninv(x, y):
+    return np.angle(x+1j*y)
+phi_angle = np.vectorize(taninv)
+
+
+# define phase function on z=0 plane, z_0=1
+def phase(phi):
+    return l*phi
+
+def lin_phase(x):
+    return x
+
+a=35
+a1=np.linspace(-a,a,500)
+xv,yv=np.meshgrid(a1,a1)
+zv=(phase(phi_angle(xv,yv))+lin_phase(xv))%(2*np.pi)
+plt.contourf(xv,yv,zv,levels=300,cmap='Greys')
+plt.xlabel("x")
+plt.ylabel("y")
+ticks = [0,np.pi,2*np.pi-0.001]
+cbar = plt.colorbar( ticks=ticks)
+cbar.set_ticklabels(['$0$', '$\pi$', '$2\pi$'])
+
+#plt.show()
+plt.savefig(f"cgh_mixed.png")
+
